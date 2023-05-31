2a3e646947e263dc6cf18a67fab21a6109ed5caa
Chris Wong <wc38.wong@gmail.com>
2023-05-31 20:29:18 +0800

    Extract a widget

--- README.md
+++ README.md
@@ -18,3 +18,4 @@ samples, guidance on mobile development, and a full API reference.
 # Your first Flutter app
 [3. Create a project](https://codelabs.developers.google.com/codelabs/flutter-codelab-first#2)  
 [4. Add a button](https://codelabs.developers.google.com/codelabs/flutter-codelab-first#3)  
+[5. Make the app prettier](https://codelabs.developers.google.com/codelabs/flutter-codelab-first#4)  
--- lib/main.dart
+++ lib/main.dart
@@ -39,12 +39,13 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
+    var pair = appState.current; // ← Add this.
 
     return Scaffold(
       body: Column(
         children: [
           Text('A random AWESOME idea:'), // ← Example change.
-          Text(appState.current.asLowerCase),
+          Text(pair.asLowerCase), // ← Change to this.
 
           // ↓ Add this.
           ElevatedButton(
