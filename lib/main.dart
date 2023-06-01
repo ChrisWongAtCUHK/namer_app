7b2a9f46c9ab70bf9910e7d38f3be9e6c7bd8884
Chris Wong <wc38.wong@gmail.com>
2023-06-01 18:48:15 +0800

    setState

--- lib/main.dart
+++ lib/main.dart
@@ -53,6 +53,8 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  var selectedIndex = 0; // ← Add this property.
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -71,9 +73,12 @@ class _MyHomePageState extends State<MyHomePage> {
                   label: Text('Favorites'),
                 ),
               ],
-              selectedIndex: 0,
+              selectedIndex: selectedIndex, // ← Change to this.
               onDestinationSelected: (value) {
-                print('selected: $value');
+                // ↓ Replace print with this.
+                setState(() {
+                  selectedIndex = value;
+                });
               },
             ),
           ),
